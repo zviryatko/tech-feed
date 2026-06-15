b5ed01104463240b6ceb316aafaae48ddc144d87
Oleksandr Davyskyba <sanya.davyskiba@gmail.com>
2026-06-15 19:41:01 +0200

    feat: add HFT University feed to build list and create site page

--- build.js
+++ build.js
@@ -28,7 +28,8 @@ const FEEDS = [
     { label: 'InfoQ', url: 'https://feed.infoq.com/' },
     { label: 'Towards Data Science', url: 'https://towardsdatascience.com/feed/' },
     { label: '@geohot', url: 'https://geohot.github.io/blog/feed.xml' },
-    { label: '@zviryatko', url: 'https://zviryatko.github.io/feed.xml' }
+    { label: '@zviryatko', url: 'https://zviryatko.github.io/feed.xml' },
+    { label: 'HFT University', url: 'https://lucisqr.substack.com/feed' }
 ];
 
 // Production feed URL (GitHub Pages)
